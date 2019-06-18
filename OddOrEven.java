aa396d61436503d904208d2deaeb63b8a18ee2df
shatdhru <42232515+shatdhruka@users.noreply.github.com>
2019-06-18 17:50:24 +0530

    Update OddOrEven.java

--- OddOrEven.java
+++ OddOrEven.java
@@ -1,16 +1,15 @@
-# even-or-odd
 import java.util.Scanner;
 public class OddOrEven
 {
    public static void main(String args[])
    {
-      int z;
-     
       Scanner in = new Scanner(System.in);
-      z = in.nextInt();
+      int z = in.nextInt();
       if ( z % 2 == 0 )
          System.out.println("Even");
-      else
+      else if (z%3==0)
          System.out.println("Odd");
+      else
+         System.out.println("Invalid");
    }
 }
